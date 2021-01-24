a4a652ae7459dfad7a7cf29ff92321cded899e33
bearhockey <bobmanworld@yahoo.com>
2021-01-24 15:43:18 -0600

    Fixed run_test script

--- run_test.sh
+++ run_test.sh
@@ -1,1 +1,3 @@
+# run this script under linux to test the application without CORS policies
+# ensure dev_chrome is in your .gitignore before comitting this
 google-chrome --disable-web-security --user-data-dir=dev_chrome test.html
